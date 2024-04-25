cc72e1f769725d84f9348a8b0593eb67b3bb3f2b
lucy726j <lucy726jeon@gmail.com>
2024-04-25 17:43:24 +0900

    [4/25] forEach로 post div 생성하기로 변경

--- post-list.js
+++ post-list.js
@@ -12,29 +12,59 @@ const postFetch = fetch("/posts.json")
   .then((response) => response.json())
   .then((data) => {
     // forEach로 post div 추가하는 방식으로 변경하기 @!
-    postList.innerHTML = Object.values(data)
-      .map(
-        (post) =>
-          ` <div class="post" id="${post.postid}">
-      <div class="title-wrap">
-        <div class="post-title">${post.title.slice(0, 26)}</div>
-        <div class="post-info">
-          <span class="post-status">
-            <span class="likes">${post.likes >= 1000 ? `좋아요 ${Math.round(post.likes / 1000)}K` : `좋아요 ${post.likes}`} </span>
-            <span class="cmts">${post.cmts >= 1 ? `댓글수 ${Math.round(post.cmts / 1000)}K` : `댓글수 ${post.cmts}`}</span>
-            <span class="views">${post.views >= 1000 ? `조회수 ${Math.round(post.views / 1000)}K` : `조회수 ${post.views}`}</span>
-          </span>
-          <span class="post-date">${post.date.year}-${post.date.month}-${post.date.day} ${post.date.hour}:${post.date.minute}:${post.date.second}</span>
-        </div>
+    //[기존!!]
+    //   postList.innerHTML = Object.values(data)
+    //     .map(
+    //       (post) =>
+    //         ` <div class="post" id="${post.postid}">
+    //     <div class="title-wrap">
+    //       <div class="post-title">${post.title.slice(0, 26)}</div>
+    //       <div class="post-info">
+    //         <span class="post-status">
+    //           <span class="likes">${post.likes >= 1000 ? `좋아요 ${Math.round(post.likes / 1000)}K` : `좋아요 ${post.likes}`} </span>
+    //           <span class="cmts">${post.cmts >= 1 ? `댓글수 ${Math.round(post.cmts / 1000)}K` : `댓글수 ${post.cmts}`}</span>
+    //           <span class="views">${post.views >= 1000 ? `조회수 ${Math.round(post.views / 1000)}K` : `조회수 ${post.views}`}</span>
+    //         </span>
+    //         <span class="post-date">${post.date.year}-${post.date.month}-${post.date.day} ${post.date.hour}:${post.date.minute}:${post.date.second}</span>
+    //       </div>
+    //     </div>
+    //     <hr />
+    //     <div class="writer-wrap">
+    //       <img src="${post.userimgurl}" alt="댓글 작성자" />
+    //       <span>${post.username}</span>
+    //     </div>
+    //   </div>`
+    //     )
+    //     .join("");
+    // [기존!!]
+
+    const postData = data;
+    console.log(postData);
+    postData.forEach((e) => {
+      const createPost = document.createElement("div");
+      createPost.classList.add("post");
+      createPost.id = e.postid;
+      createPost.innerHTML = `<div class="title-wrap">
+      <div class="post-title">${e.title.slice(0, 26)}</div>
+      <div class="post-info">
+        <span class="post-status">
+          <span class="likes">${e.likes >= 1000 ? `좋아요 ${Math.round(e.likes / 1000)}K` : `좋아요 ${e.likes}`} </span>
+          <span class="cmts">${e.cmts >= 1 ? `댓글수 ${Math.round(e.cmts / 1000)}K` : `댓글수 ${e.cmts}`}</span>
+          <span class="views">${e.views >= 1000 ? `조회수 ${Math.round(e.views / 1000)}K` : `조회수 ${e.views}`}</span>
+        </span>
+        <span class="post-date">${e.date.year}-${e.date.month}-${e.date.day} ${e.date.hour}:${e.date.minute}:${e.date.second}</span>
       </div>
-      <hr />
-      <div class="writer-wrap">
-        <img src="${post.userimgurl}" alt="댓글 작성자" />
-        <span>${post.username}</span>
-      </div>
-    </div>`
-      )
-      .join("");
+    </div>
+
+    <hr />
+
+    <div class="writer-wrap">
+      <img src="${e.userimgurl}" alt="댓글 작성자" />
+      <span></span>
+    </div>`;
+      const posts = document.querySelector(".posts");
+      posts.appendChild(createPost);
+    });
   });
 
 // [NOTE] 포스트 전체가 하나의 링크로만..
